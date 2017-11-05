eea865fbad36f2bf9ec9d9358bf112a3ab2d18bf
Rian Ruggles <rs.ruggles@gmail.com>
2017-11-05 17:52:51 -0600

    Refactor API with underscore

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -13,31 +14,34 @@ app.get('/', function (req, res) {
   res.send('Todo API Root');
 });
 
+// GET /todos
 app.get('/todos', function (req, res) {
   res.json(todos);
 });
 
+// GET /todos/:id
 app.get('/todos/:id', function (req, res) {
   var todoID = parseInt(req.params.id, 10);
-  var matchedTodo;
+  var matchedTodo = _.findWhere(todos, {id: todoID});
   
-  todos.forEach(function (todo) {
-    if (todoID === todo.id) {
-      matchedTodo = todo;
-    }
-  });
   
   if (matchedTodo) {
     res.json(matchedTodo);
   } else {
     res.status(404).send();
   }
-
 });
 
+// POST /todos
 app.post('/todos', function (req, res) {
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
 
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+  
+  body.description = body.description.trim();
+  
   body.id = todoNextID++;
 
   todos.push(body);
