186dccf36271f681ae0a28ef98abc9e028bc8e8d
Rian Ruggles <rs.ruggles@gmail.com>
2017-11-05 19:22:29 -0600

    Add PUT todos/:id

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*jslint devel: true, node: true, eqeq: true, nomen: true, plusplus: true, regexp: true, sloppy: true, vars: true*/
+/*jslint devel: true, node: true, eqeq: true, nomen: true, plusplus: true, regexp: true, sloppy: true, vars: true, es5: true*/
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -60,6 +60,33 @@ app.delete('/todos/:id', function (req, res) {
     todos = _.without(todos, matchedTodo);
     res.json(matchedTodo);
   }
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+  var todoID = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoID});
+  var body = _.pick(req.body, 'description', 'completed');
+  var validAttributes = {};
+  
+  if (!matchedTodo) {
+    return res.status(404).send();
+  }
+  
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')) {
+    return res.status(404).send();
+  }
+  
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+    validAttributes.description = body.description;
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(404).send();
+  }
+  
+  _.extend(matchedTodo, validAttributes);  
+  res.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
