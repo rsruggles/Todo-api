876533ddebf35983dbde7c58d351f383acefdaab
Rian Ruggles <rs.ruggles@gmail.com>
2017-11-07 20:01:59 -0600

    Validate duplicated emails with case sensitivity

--- models/user.js
+++ models/user.js
@@ -15,5 +15,14 @@ module.exports = function (sequelize, DataTypes) {
         len: [8, 100]
       }
     }
-  })
+  }, {
+    hooks: {
+      beforeValidate: function (user,options) {
+        // user.email to lowercase
+        if (typeof user.email === 'string') {
+          user.email = user.email.toLowerCase();
+        }
+      }
+    }
+  });
 };
